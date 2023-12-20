e85e32a759ed0b437dc20dffa89de66da1e495ed
frnot <frnot@frnot.com>
2023-12-20 01:26:46 -0600

    add dotfile sync to setup script

--- .config/setup.sh
+++ .config/setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Sync dotfiles
+alias dotgit='/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME'
+git clone --bare https://github.com/Frnot/dotfiles $HOME/.dotfiles.git
+dotgit config status.showUntrackedFiles no # if the config is synced, this may be unecessary to run every time
+dotgit checkout  # this may fail, is there a FORCE flag?
+
 # Setup samba share 
 echo "Configuring samba share"
 read -sp "Password for frnot@fileserver.lab.frnot.com: " smbpasswd
